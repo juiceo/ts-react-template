8daf46ef862bca87ac082e9d3d4f7f232d5dcbb6
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2020-03-08 03:06:09 +0200

    Update workers site routing

--- workers-site/index.js
+++ workers-site/index.js
@@ -10,52 +10,65 @@ import { getAssetFromKV, mapRequestToAsset } from '@cloudflare/kv-asset-handler'
 const DEBUG = false
 
 addEventListener('fetch', event => {
-  try {
-    event.respondWith(handleEvent(event))
-  } catch (e) {
-    if (DEBUG) {
-      return event.respondWith(
-        new Response(e.message || e.toString(), {
-          status: 500,
-        }),
-      )
+    try {
+        event.respondWith(handleEvent(event))
+    } catch (e) {
+        if (DEBUG) {
+            return event.respondWith(
+                new Response(e.message || e.toString(), {
+                    status: 500,
+                })
+            )
+        }
+        event.respondWith(new Response('Internal Error', { status: 500 }))
     }
-    event.respondWith(new Response('Internal Error', { status: 500 }))
-  }
 })
 
 async function handleEvent(event) {
-  const url = new URL(event.request.url)
-  let options = {}
+    const url = new URL(event.request.url)
+    let options = {}
 
-  /**
-   * You can add custom logic to how we fetch your assets
-   * by configuring the function `mapRequestToAsset`
-   */
-  // options.mapRequestToAsset = handlePrefix(/^\/docs/)
+    /**
+     * You can add custom logic to how we fetch your assets
+     * by configuring the function `mapRequestToAsset`
+     */
+    options.mapRequestToAsset = req => {
+        // First let's apply the default handler, which we imported from
+        // '@cloudflare/kv-asset-handler' at the top of the file. We do
+        // this because the default handler already has logic to detect
+        // paths that should map to HTML files, for which it appends
+        // `/index.html` to the path.
+        req = mapRequestToAsset(req)
 
-  try {
-    if (DEBUG) {
-      // customize caching
-      options.cacheControl = {
-        bypassCache: true,
-      }
+        // Now we can detect if the default handler decided to map to
+        // index.html in some specific directory.
+        if (req.url.endsWith('/index.html')) {
+            // Indeed. Let's change it to instead map to the root `/index.html`.
+            // This avoids the need to do a redundant lookup that we know will
+            // fail.
+            return new Request(`${new URL(req.url).origin}/index.html`, req)
+        } else {
+            // The default handler decided this is not an HTML page. It's probably
+            // an image, CSS, or JS file. Leave it as-is.
+            return req
+        }
     }
-    return await getAssetFromKV(event, options)
-  } catch (e) {
-    // if an error is thrown try to serve the asset at 404.html
-    if (!DEBUG) {
-      try {
-        let notFoundResponse = await getAssetFromKV(event, {
-          mapRequestToAsset: req => new Request(`${new URL(req.url).origin}/404.html`, req),
+
+    try {
+        if (DEBUG) {
+            // customize caching
+            options.cacheControl = {
+                bypassCache: true,
+            }
+        }
+        return await getAssetFromKV(event, options)
+    } catch (e) {
+        // Fall back to serving `/index.html` on errors.
+        return getAssetFromKV(event, {
+            mapRequestToAsset: req =>
+                new Request(`${new URL(req.url).origin}/index.html`, req),
         })
-
-        return new Response(notFoundResponse.body, { ...notFoundResponse, status: 404 })
-      } catch (e) {}
     }
-
-    return new Response(e.message || e.toString(), { status: 500 })
-  }
 }
 
 /**
@@ -66,15 +79,15 @@ async function handleEvent(event) {
  * to exist at a specific path.
  */
 function handlePrefix(prefix) {
-  return request => {
-    // compute the default (e.g. / -> index.html)
-    let defaultAssetKey = mapRequestToAsset(request)
-    let url = new URL(defaultAssetKey.url)
+    return request => {
+        // compute the default (e.g. / -> index.html)
+        let defaultAssetKey = mapRequestToAsset(request)
+        let url = new URL(defaultAssetKey.url)
 
-    // strip the prefix from the path for lookup
-    url.pathname = url.pathname.replace(prefix, '/')
+        // strip the prefix from the path for lookup
+        url.pathname = url.pathname.replace(prefix, '/')
 
-    // inherit all other props from the default request
-    return new Request(url.toString(), defaultAssetKey)
-  }
+        // inherit all other props from the default request
+        return new Request(url.toString(), defaultAssetKey)
+    }
 }
